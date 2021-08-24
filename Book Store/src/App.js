c0e3dfba63f4141106e377fc37b0e5e697c21cea
Suraj-Sharma-2404p <86391485+Suraj-Sharma-2404p@users.noreply.github.com>
2021-08-24 07:55:04 +0530

    Update App.js

--- Book Store/src/App.js
+++ Book Store/src/App.js
@@ -1,19 +1,13 @@
 import React from "react";
 import "./App.css";
-// import Counter from "./components/Hooks/counterpractice/Counter";
 import Onlinebookstore from "./components/Hooks/opscube/Onlinebookstore";
 
-// import UseState from './components/Hooks/UseState'
-// import UseEffect from "./components/Hooks/UseEffect"
-// import UseReducer from "./components/Hooks/UseReducer";
+
 
 const App = () => {
   return (
     <div>
-      {/* <UseState/> */}
-      {/* <UseEffect/> */}
-      {/* <UseReducer/> */}
-      {/* <Counter/> */}
+      
       <Onlinebookstore/>
     </div>
   );
